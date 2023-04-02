9686080e6451482885ff8e5e7cf2b3944ffd4fca
Daniel Mark Courtney <dan4310@gmail.com>
2023-04-02 13:22:07 -0400

    fix(scripts): update blocks scripts

--- blocks.def.h
+++ blocks.def.h
@@ -1,21 +1,21 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "sb-memory",	5,		0},
+	{"", "sb-memory",	5,		0},
 
-	{"Task: ", "sb-tasks", 5, 0},
+	{"", "sb-tasks", 5, 0},
 
-	{"Pac", "sb-pacpackages", 60, 0},
+	{"", "sb-pacpackages", 60, 0},
 
-	{"", "sb-forecast", 60*60},
+	{"", "sb-forecast", 60*60, 15},
 
 	{"", "sb-clock",					5,		0},
 
-	{"Volume: ", "vol get", 0, 10},
+	{"", "sb-volume", 0, 10},
 	
-	{"Wifi: ", "sb-internet", 5, 0}
+	{"", "sb-internet", 5, 0}
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = "  ";
 static unsigned int delimLen = 5;
