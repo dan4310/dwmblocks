b839be190dcf42bd8d375c4024083a2452897ace
Daniel Mark Courtney <dan4310@gmail.com>
2023-04-03 00:05:12 -0400

    feat(music): add music block

--- blocks.def.h
+++ blocks.def.h
@@ -1,7 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"", "sb-music", 5, 30},
+
 	{"", "sb-cpu", 5, 20 },
+
 	{"", "sb-memory",	5,		0},
 
 	{"", "sb-tasks", 5, 25},
